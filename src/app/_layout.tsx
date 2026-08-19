04c9886aa2d5b01c01f407e546662b36201008c8
Marvin Stickel <marv.stickel@gmail.com>
2026-08-19 22:56:23 +0200

    Chore: Setup dev tools plugins

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,10 @@
-import { Stack } from "expo-router";
+import { useReactNavigationDevTools } from "@dev-plugins/react-navigation";
+import { Stack, useNavigationContainerRef } from "expo-router";
 
 export default function RootLayout() {
+  const navigationRef = useNavigationContainerRef();
+
+  useReactNavigationDevTools(navigationRef);
+
   return <Stack />;
 }
